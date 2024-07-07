3ca2b70605ed9d39f16bd87221a75c289ca933ac
YuichiKurihara <coelacanth_616@yahoo.co.jp>
2024-07-07 15:48:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import streamlit as st
 import numpy as np
 import cv2
+import time
+from PIL import Image
+
 
 st.title("AI変身ミラー(α)")
 
-img = np.zeros((500, 500, 3), np.uint8)
-cv2.rectangle(img, (100, 100), (400, 400), color=(255, 0, 0), thickness=-1)
+device = user_input = st.text_input("input your video/camera device", "0")
+if device.isnumeric():
+    # e.g. "0" -> 0
+    device = int(device)
 
-st.image(img)
+cap = cv2.VideoCapture(device)
+
+image_loc = st.empty()
+while cap.isOpened:
+    ret, img = cap.read()
+    time.sleep(0.01)
+    img = Image.fromarray(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    image_loc.image(img)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
